093b437ae84a11dfa46a524e7e3c2fcaf848358c
mqhtn <mh.truongngoc@gmail.com>
2025-11-08 15:22:29 +0100

    Add i18n support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ['de', 'vi'],
+    defaultLocale: 'de',
+  },
 };
 
 export default nextConfig;
