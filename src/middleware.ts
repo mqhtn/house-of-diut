d5f28bfdc2a29db69d25b5614d77422815018144
mqhtn <mh.truongngoc@gmail.com>
2025-11-10 12:48:02 +0100

    Revert proxy settings

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname
+
+  // If pathname is the root (/) exactly
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/de', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
